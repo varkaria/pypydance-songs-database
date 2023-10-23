d1b27e81e68486e64638e2fbf7a2c071e9ab93c1
Pavaresorn Malai <61514399+Varkaria@users.noreply.github.com>
2023-10-24 00:42:12 +0700

    fix(web): prevent mobile auto zoom from focus input

--- src/modules/shareds/Layout.tsx
+++ src/modules/shareds/Layout.tsx
@@ -12,6 +12,10 @@ export const Layout = (props: { children: React.ReactNode }) => {
           name="description"
           content="a songs database seacher for pypydance worlds"
         />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        />
         <link rel="icon" href="/favicon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
