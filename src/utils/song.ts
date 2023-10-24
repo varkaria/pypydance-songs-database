0dae79d4c919181ee25aae2e58a20fe9f79931b7
Pavaresorn Malai <61514399+Varkaria@users.noreply.github.com>
2023-10-24 16:52:45 +0700

    feat(web): implement fitness marshall metadata
    not fully for now

--- src/utils/song.ts
+++ src/utils/song.ts
@@ -1,6 +1,13 @@
+import { artists } from "@/data/artists";
 import { Group } from "@/utils/hooks/usePypySongs";
 
+
 export const getSongMetadata = (metadata: string, group: Group) => {
+  const defaultReturn = {
+    title: metadata,
+    artist: '',
+  }
+
   switch (group) {
     case Group.FITDANCE:
       return {
@@ -12,10 +19,22 @@ export const getSongMetadata = (metadata: string, group: Group) => {
         title: metadata.split(']')[1],
         artist: metadata.split('[')[1]?.split(']')[0],
       };
-    default:
+    case Group.FITNESSMARSHALL:
+      if (!metadata.includes(' - ')) return defaultReturn
+
+      // because some songs have the artist name first
+      if (artists.includes(metadata.split(' - ')[1] ?? "")) {
+        return {
+          title: metadata.split(' - ')[0],
+          artist: metadata.split(' - ')[1],
+        }
+      }
+
       return {
-        title: metadata,
-        artist: '',
-      };
+        title: metadata.split(' - ')[1],
+        artist: metadata.split(' - ')[0],
+      }
+    default:
+      return defaultReturn
   }
 }
