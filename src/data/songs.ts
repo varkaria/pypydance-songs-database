97a73085d2ef5ce89801f31560aa7f5b2e745e7e
Pavaresorn Malai <61514399+Varkaria@users.noreply.github.com>
2023-11-01 02:29:47 +0700

    feat(web): finishing fitness marshall metadata

--- src/data/songs.ts
+++ src/data/songs.ts
@@ -15,5 +15,338 @@ export const songsMetadata: {
           artist: "aespa",
         }
       }
+    },
+    {
+      id: 330,
+      data: {
+        metadata: {
+          title: "Special",
+          artist: "Keaira LaShae"
+        }
+      }
+    },
+    {
+      id: 477,
+      data: {
+        metadata: {
+          title: "Get Low",
+          artist: "Liam Payne"
+        }
+      }
+    },
+    {
+      id: 219,
+      data: {
+        metadata: {
+          title: "birthday cake",
+          artist: "Dylan Conrique"
+        }
+      }
+    },
+    {
+      id: 313,
+      data: {
+        metadata: {
+          title: "If U Seek Amy",
+          artist: "Britney Spears"
+        }
+      }
+    },
+    {
+      id: 226,
+      data: {
+        metadata: {
+          title: "Break Free (feat. Zedd)",
+          artist: "Ariana Grande"
+        }
+      }
+    },
+    {
+      id: 305,
+      data: {
+        metadata: {
+          title: "DESSERT (feat. Loopy & SOYEON)",
+          artist: "HYO"
+        }
+      }
+    },
+    {
+      id: 287,
+      data: {
+        metadata: {
+          title: "Flawless Remix (feat. Nicki Minaj)",
+          artist: "Beyoncé"
+        }
+      }
+    },
+    {
+      id: 417,
+      data: {
+        metadata: {
+          title: "Side Bitch Issues",
+          artist: "Brandon Beal"
+        }
+      }
+    },
+    {
+      id: 258,
+      data: {
+        metadata: {
+          title: "Diva Yorgun",
+          artist: "Melike Şahin"
+        }
+      }
+    },
+    {
+      id: 432,
+      data: {
+        metadata: {
+          title: "Sweat On Me (feat. Vicki Vox)",
+          artist: "Tape Machines"
+        }
+      }
+    },
+    {
+      id: 280,
+      data: {
+        metadata: {
+          title: "Feelin' Myself (feat. Miley Cyrus, French Montana, Wiz Khalifa & Mustard)",
+          artist: "will.i.am"
+        }
+      }
+    },
+    {
+      id: 357,
+      data: {
+        metadata: {
+          title: "Make It Nasty",
+          artist: "Tyga"
+        }
+      }
+    },
+    {
+      id: 314,
+      data: {
+        metadata: {
+          title: "In My Trunk (feat. Maxo Kream & Young Dolph)",
+          artist: "Bun B"
+        }
+      }
+    },
+    {
+      id: 171,
+      data: {
+        metadata: {
+          title: "Life's Too Short",
+          artist: "aespa"
+        }
+      }
+    },
+    {
+      id: 388,
+      data: {
+        metadata: {
+          title: "Pretty Girls",
+          artist: "Reneé Rapp"
+        }
+      }
+    },
+    {
+      id: 186,
+      data: {
+        metadata: {
+          title: "23 (feat. Miley Cyrus, Wiz Khalifa & Juicy J)",
+          artist: "Mike WiLL Made-It"
+        }
+      }
+    },
+    {
+      id: 206,
+      data: {
+        metadata: {
+          title: "Ayy Ladies (feat. Tyga)",
+          artist: "Travis Porter"
+        }
+      }
+    },
+    {
+      id: 402,
+      data: {
+        metadata: {
+          title: "Salt Shaker (feat. Lil Jon And The Eastside Boyz)",
+          artist: "Ying Yang Twins"
+        }
+      }
+    },
+    {
+      id: 284,
+      data: {
+        metadata: {
+          title: "Figure 8",
+          artist: "Paramore"
+        }
+      }
+    },
+    {
+      id: 210,
+      data: {
+        metadata: {
+          title: "Back It Up",
+          artist: "Dadju"
+        }
+      }
+    },
+    {
+      id: 400,
+      data: {
+        metadata: {
+          title: "S&M Remix (feat. Britney Spears)",
+          artist: "Rihanna"
+        }
+      }
+    },
+    {
+      id: 458,
+      data: {
+        metadata: {
+          title: "Twerk It Like Miley (feat. Christopher)",
+          artist: "Brandon Beal"
+        }
+      }
+    },
+    {
+      id: 355,
+      data: {
+        metadata: {
+          title: "Love Me Harder",
+          artist: "Ariana Grande"
+        }
+      }
+    },
+    {
+      id: 376,
+      data: {
+        metadata: {
+          title: "Numb",
+          artist: "Linkin Park"
+        }
+      }
+    },
+    {
+      id: 252,
+      data: {
+        metadata: {
+          title: "Dance Like We're Making Love",
+          artist: "Nicky B"
+        }
+      }
+    },
+    {
+      id: 193,
+      data: {
+        metadata: {
+          title: "Me Gusta (feat. Cardi B & Myke Towers)",
+          artist: "Anitta"
+        }
+      }
+    },
+    {
+      id: 439,
+      data: {
+        metadata: {
+          title: "The Gym Beats Vol. 4-Nonstop-Megamix",
+          artist: "THE GYM BEATS"
+        }
+      }
+    },
+    {
+      id: 292,
+      data: {
+        metadata: {
+          title: "Gimme More",
+          artist: "Britney Spears"
+        }
+      }
+    },
+    {
+      id: 315,
+      data: {
+        metadata: {
+          title: "Inside Out",
+          artist: "Britney Spears"
+        }
+      }
+    },
+    {
+      id: 220,
+      data: {
+        metadata: {
+          title: "Bitch I'm Madonna (feat. Nicki Minaj)",
+          artist: "Madonna"
+        }
+      }
+    },
+    {
+      id: 387,
+      data: {
+        metadata: {
+          title: "Pound The Alarm",
+          artist: "Nicki Minaj"
+        }
+      }
+    },
+    {
+      id: 264,
+      data: {
+        metadata: {
+          title: "Drive",
+          artist: "Miley Cyrus"
+        }
+      }
+    },
+    {
+      id: 308,
+      data: {
+        metadata: {
+          title: "I KNOW ?",
+          artist: "Travis Scott"
+        }
+      }
+    },
+    {
+      id: 453,
+      data: {
+        metadata: {
+          title: "Touchin, Lovin (feat. Nicki Minaj)",
+          artist: "Trey Songz"
+        }
+      }
+    },
+    {
+      id: 394,
+      data: {
+        metadata: {
+          title: "Ride",
+          artist: "Sir Mix-A-Lot"
+        }
+      }
+    },
+    {
+      id: 223,
+      data: {
+        metadata: {
+          title: "Bongo Cha Cha Cha",
+          artist: "Goodboys"
+        }
+      }
+    },
+    {
+      id: 179,
+      data: {
+        metadata: {
+          title: "Whip It",
+          artist: "Devo"
+        }
+      }
     }
   ]
